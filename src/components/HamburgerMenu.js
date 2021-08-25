1f4ebe244ca0c81c9a2b129ccb556705acaa0e90
Corneliaryas <hello@corneliaryas.com>
2021-08-25 17:13:08 +0200

    Hamburger animation

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -5,17 +5,9 @@ export const HamburgerMenu = ({showMenu, setShowMenu}) => {
 
     return (
         <Hamburger onClick={() => setShowMenu(!showMenu)}>
-            {!showMenu && <>
-            <Line></Line>
-            <Line></Line>
-            <Line></Line>
-            </>
-        }
-        {showMenu && <>
-        <Cross1></Cross1>
-        <Cross2></Cross2>
-        </>
-        }
+            <Line1 active={showMenu}></Line1>
+            <Line2 active={showMenu}></Line2>
+            <Line3 active={showMenu}></Line3>
         </Hamburger>
     )
 }
@@ -26,34 +18,41 @@ width: 40px;
 height: 30px;
 display: none;
 cursor: pointer;
+justify-content: center;
 @media screen and (max-width: 768px) {
-  display: block;
+  display: flex;
   }
 `
 
-const Line = styled.div`
+const Line1 = styled.div`
+position: absolute;
+top: ${props => props.active ? "10px" : "0"};
 width: 100%;
 height: 4px;
 border-radius: 50px;
 background-color: black;
-margin: 5px;`
+margin: 5px;
+transform: ${props => props.active ? "rotate(45deg)" : "rotate(0)"};
+transition: 0.3s;`
 
-const Cross1 = styled.div`
+const Line2 = styled.div`
 position: absolute;
+width: ${props => props.active ? "0" : "100%"};
 top: 10px;
-width: 80%;
 height: 4px;
 border-radius: 50px;
 background-color: black;
 margin: 5px;
-transform: rotate(45deg);`
+transition: 0.3s;`
 
-const Cross2 = styled.div`
+const Line3 = styled.div`
 position: absolute;
-top: 10px;
-width: 80%;
+top: ${props => props.active ? "10px" : "20px"};
+width: 100%;
 height: 4px;
 border-radius: 50px;
 background-color: black;
 margin: 5px;
-transform: rotate(-45deg);`
+transform: ${props => props.active ? "rotate(-45deg)" : "rotate(0)"};
+transition: 0.3s;`
+
